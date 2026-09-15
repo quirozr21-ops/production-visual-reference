a9b72658311c55d67ee8c5e652b4a63e40a83ef1
quirozr21-ops <quiroz.r21@gmail.com>
2026-09-15 09:52:21 -0700

    Fix 12 MB image upload request limit

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   poweredByHeader: false,
   experimental: {
+    proxyClientMaxBodySize: "16mb",
     serverActions: {
-      bodySizeLimit: "13mb",
+      bodySizeLimit: "16mb",
     },
   },
 };
